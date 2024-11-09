72f1a5d4f27ca7ea721fe6988b31193072f5b8a2
Jose Carlos <jyon@isise.edu.pe>
2024-11-08 19:21:23 -0500

    Semana 1 - Clase 1

---
+++ WPF-Clase01/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Clase01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            if(username == "admin" && password == "123")
+            {
+                MessageBox.Show($"Login Success con el usuario: {username}");
+                // Console.WriteLine($"Login Success con el usuario: {username}");
+            }
+            else
+            {
+                MessageBox.Show("Login Fallido");
+                // Console.WriteLine("Login Fallido");
+            }
+        }
+    }
+}
